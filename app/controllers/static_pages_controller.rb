827013143f29fae9fb79ec9cb53a71bb88cb480f
cassiemak <cassie.mak@gmail.com>
2014-12-10 09:45:31 +0800

    intro

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,14 @@
+class StaticPagesController < ApplicationController
+  def index
+    # we dont have any logics here
+    # so we are just going to render the page 'index.html'
+  end
+
+  def mathgame
+
+  end
+
+  def cassie
+
+  end
+end
