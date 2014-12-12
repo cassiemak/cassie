6a35e8c0b4173271af840b02cebdf6fb2df73ecf
cassiemak <cassie.mak@gmail.com>
2014-12-13 01:20:06 +0800

    footer color

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,13 @@
 //= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
+// added for google map background?
+$(document).ready(function() {
+ var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
